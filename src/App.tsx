9ef200898dd31eefa756817d08fefd2e65797209
Juan Cruz  Avellaneda <juanxavellaneda@gmail.com>
2025-07-03 23:58:40 +0200

    Polished some visual details

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import './App.css';
+import styles from './App.module.css';
 import { CanvasSlider } from './components/CanvasSlider';
 
 import img0 from './assets/0.jpg';
@@ -8,13 +8,15 @@ import img3 from './assets/3.jpg';
 
 function App() {
   return (
-    <main id="root">
+    <main className={styles.root}>
+      <h1>React Canvas Image Slider</h1>
       <CanvasSlider width={640} height={400}>
         <CanvasSlider.Image src={img0} />
         <CanvasSlider.Image src={img1} />
         <CanvasSlider.Image src={img2} />
         <CanvasSlider.Image src={img3} />
       </CanvasSlider>
+      <small>Drag to change image</small>
     </main>
   );
 }
